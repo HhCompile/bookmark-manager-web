40fd9e5e81ed31b8a09ee48c7bcb7cd8c75080cb
Mr.sun <hh@MrsundeMac-mini.local>
2026-01-04 15:46:59 +0800

    initial commit

--- bookmark-manager/tests/uploadpage-render.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from 'vitest'
-import { render } from '@testing-library/react'
-import UploadPage from '../src/pages/UploadPage'
-
-test('UploadPage renders without crashing', () => {
-  render(<UploadPage />)
-  expect(true).toBe(true)
-})
